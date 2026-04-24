b2e98a760f715a778916fb6f507d92d8b718ea29
mrip-dev <im950959@gmail.com>
2026-04-24 14:28:18 +0500

    CORS for plantyc

--- config/cors.php
+++ config/cors.php
@@ -26,6 +26,7 @@ return [
         'http://127.0.0.1:3000',
         'http://127.0.0.1:3001',
         'http://127.0.0.1:5173',
+        'https://plantiyc-task.vercel.app',
     ],
 
     'allowed_origins_patterns' => [
